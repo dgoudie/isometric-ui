cc8af25729fdbc0c7c2194f82de93aa0faebd89e
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-13 00:15:23 -0600

    one more try

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -22,16 +22,44 @@ clientsClaim();
 const precacheController = new PrecacheController();
 precacheController.precache(self.__WB_MANIFEST);
 
+// Set up App Shell-style routing, so that all navigation requests
+// are fulfilled with your index.html shell. Learn more at
+// https://developers.google.com/web/fundamentals/architecture/app-shell
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const shouldBeRoutedToIndexHtml = (request: Request, url: URL) => {
+    if (url.pathname === '/index.html') {
+        return true;
+    }
+
+    if (request.mode !== 'navigate') {
+        return false;
+    }
+
+    // If this is a URL that starts with /_, skip.
+    if (url.pathname.startsWith('/_')) {
+        return false;
+    }
+
+    // If this looks like a URL for a resource, because it contains
+    // a file extension, skip.
+    if (url.pathname.match(fileExtensionRegexp)) {
+        return false;
+    }
+
+    // Return true to signal that we want to use the handler.
+    return true;
+};
+
 self.addEventListener('fetch', (event) => {
     const { request } = event;
     const url = new URL(request.url);
-    if (url.origin === location.origin && url.pathname === '/index.html') {
+    if (shouldBeRoutedToIndexHtml(request, url)) {
         fetch(request, { mode: 'no-cors' }).then((fetchResponse) => {
             if (fetchResponse.redirected) {
                 event.respondWith(fetchResponse);
             } else {
                 const cacheKey = precacheController.getCacheKeyForURL(
-                    event.request.url
+                    process.env.PUBLIC_URL + '/index.html'
                 );
                 if (!!cacheKey) {
                     //@ts-ignore
@@ -49,37 +77,6 @@ self.addEventListener('fetch', (event) => {
         }
     }
 });
-
-// Set up App Shell-style routing, so that all navigation requests
-// are fulfilled with your index.html shell. Learn more at
-// https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-registerRoute(
-    // Return false to exempt requests from being fulfilled by index.html.
-    ({ request, url }: { request: Request; url: URL }) => {
-        // If this isn't a navigation, skip.
-        if (request.mode !== 'navigate') {
-            return false;
-        }
-
-        // If this is a URL that starts with /_, skip.
-        if (url.pathname.startsWith('/_')) {
-            return false;
-        }
-
-        // If this looks like a URL for a resource, because it contains
-        // a file extension, skip.
-        if (url.pathname.match(fileExtensionRegexp)) {
-            return false;
-        }
-
-        // Return true to signal that we want to use the handler.
-        return true;
-    },
-    precacheController.createHandlerBoundToURL(
-        process.env.PUBLIC_URL + '/index.html'
-    )
-);
 
 registerRoute(
     ({ url, sameOrigin }) => sameOrigin && url.pathname === '/api/exercises',
