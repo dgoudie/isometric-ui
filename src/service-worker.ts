600bb1ba98eef1f95982b5374431c582954b3f0d
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-12 21:07:00 -0600

    added cache for google fonts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -8,12 +8,12 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 
 import { ExpirationPlugin } from 'workbox-expiration';
 import { clientsClaim } from 'workbox-core';
-import { registerRoute } from 'workbox-routing';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -25,64 +25,40 @@ clientsClaim();
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Set up App Shell-style routing, so that all navigation requests
-// are fulfilled with your index.html shell. Learn more at
-// https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const handler = createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html');
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
+
 registerRoute(
-    // Return false to exempt requests from being fulfilled by index.html.
-    ({ request, url }: { request: Request; url: URL }) => {
-        // If this isn't a navigation, skip.
-        if (request.mode !== 'navigate') {
-            return false;
-        }
-
-        // If this is a URL that starts with /_, skip.
-        if (url.pathname.startsWith('/_')) {
-            return false;
-        }
-
-        // If this looks like a URL for a resource, because it contains
-        // a file extension, skip.
-        if (url.pathname.match(fileExtensionRegexp)) {
-            return false;
-        }
-
-        // Return true to signal that we want to use the handler.
-        return true;
-    },
-    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
-);
-
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
-registerRoute(
-    // Add in any other file extensions or routing criteria as needed.
-    ({ url }) => url.pathname.endsWith('.png') || url.pathname.endsWith('.ico'),
-    // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    /.*\.(?:ico|png)$/gi,
     new StaleWhileRevalidate({
         cacheName: 'images',
-        plugins: [
-            // Ensure that once this runtime cache reaches a maximum size the
-            // least-recently used images are removed.
-            new ExpirationPlugin({ maxEntries: 50 }),
-        ],
+        plugins: [new ExpirationPlugin({ maxEntries: 50 })],
     })
 );
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-    // Add in any other file extensions or routing criteria as needed.
-    ({ url, sameOrigin }) => sameOrigin && url.pathname.startsWith('/api'),
-    // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    ({ url, sameOrigin }) => sameOrigin && url.pathname === '/api/exercises',
     new NetworkFirst({
-        cacheName: 'data',
-        plugins: [
-            // Ensure that once this runtime cache reaches a maximum size the
-            // least-recently used images are removed.
-            new ExpirationPlugin({ maxEntries: 50 }),
-        ],
+        cacheName: 'exercise',
+        plugins: [new ExpirationPlugin({ maxEntries: 1 })],
+    })
+);
+
+registerRoute(
+    ({ url, sameOrigin }) =>
+        sameOrigin && url.pathname.startsWith('/api/exercise/'),
+    new NetworkFirst({
+        cacheName: 'exercises',
+        plugins: [new ExpirationPlugin({ maxEntries: 50 })],
+    })
+);
+
+registerRoute(
+    ({ url }) => url.host === 'fonts.gstatic.com',
+    new StaleWhileRevalidate({
+        cacheName: 'google_fonts',
+        plugins: [new ExpirationPlugin({ maxEntries: 10 })],
     })
 );
 
