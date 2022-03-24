f60487e2917f34cec98fddb2a22706c13e22290a
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-23 20:12:00 -0500

    middle of splitting editor

--- src/components/WorkoutPlanEditor/components/WorkoutPlanDayEditor.tsx
+++ src/components/WorkoutPlanEditor/components/WorkoutPlanDayEditor.tsx
@@ -7,156 +7,10 @@ import {
 import { IExercise, IWorkoutScheduleDay } from '@dgoudie/isometric-types';
 import { ObjectID, ObjectId } from 'bson';
 import React, { useCallback, useState } from 'react';
-import {
-    deleteItemFromArray,
-    moveItemInArray,
-    replaceItemInArray,
-} from '../../utils/array-helpers';
 
-import CopyDayBottomSheet from '../CopyDayBottomSheet/CopyDayBottomSheet';
-import ExercisePickerBottomSheet from '../ExercisePickerBottomSheet/ExercisePickerBottomSheet';
-import MuscleGroupTag from '../MuscleGroupTag/MuscleGroupTag';
-import styles from './WorkoutPlanEditor.module.scss';
+import styles from './WorkoutPlanDayEditor.module.scss';
 
 interface Props {
-    days: IWorkoutScheduleDay[];
-    daysChanged: (days: IWorkoutScheduleDay[]) => void;
-    exerciseMap: Map<string, IExercise>;
-    dayReorderModeEnabled?: boolean;
-}
-
-export default function WorkoutPlanEditor({
-    days,
-    exerciseMap,
-    daysChanged,
-    dayReorderModeEnabled = false,
-}: Props) {
-    const [copyDayVisible, setCopyDayVisible] = useState(false);
-
-    const updateAndReportDays = useCallback(
-        (updatedDays: typeof days) => {
-            // setDays(updatedDays);
-            daysChanged(updatedDays);
-        },
-        [daysChanged]
-    );
-
-    const onDragEnd = useCallback(
-        ({ source, destination }: DropResult) => {
-            if (!destination) {
-                return;
-            }
-            if (destination.index === source.index) {
-                return;
-            }
-            updateAndReportDays(
-                moveItemInArray(days, source.index, destination.index)
-            );
-        },
-        [days, updateAndReportDays]
-    );
-
-    const handleAdd = useCallback(() => {
-        updateAndReportDays([
-            ...days,
-            { exercises: [], nickname: '', _id: new ObjectID().toString() },
-        ]);
-    }, [days, updateAndReportDays]);
-
-    const handleDelete = useCallback(
-        (index: number) => {
-            updateAndReportDays(deleteItemFromArray(days, index));
-        },
-        [days, updateAndReportDays]
-    );
-
-    const dayChanged = useCallback(
-        (day: IWorkoutScheduleDay, index: number) => {
-            updateAndReportDays(replaceItemInArray(days, index, day));
-        },
-        [days, updateAndReportDays]
-    );
-
-    const onCopyDayResult = useCallback(
-        (result: number | undefined) => {
-            if (typeof result !== 'undefined') {
-                const day = days[result];
-                daysChanged([
-                    ...days,
-                    { ...day, _id: new ObjectId().toString() },
-                ]);
-            }
-            setCopyDayVisible(false);
-        },
-        [days, daysChanged]
-    );
-
-    if (!days.length) {
-        return (
-            <div className={styles.noDays}>
-                <span>
-                    Here, you can build a workout schedule. Start by adding a
-                    day, and adding some exercises.
-                </span>
-                <button
-                    className={'standard-button primary'}
-                    onClick={handleAdd}
-                >
-                    <i className='fa-solid fa-plus'></i>
-                    Add Day
-                </button>
-            </div>
-        );
-    }
-
-    return (
-        <div className={styles.list}>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable droppableId='workout_plan_days'>
-                    {(provided) => (
-                        <div
-                            ref={provided.innerRef}
-                            {...provided.droppableProps}
-                        >
-                            {days.map((day, index) => (
-                                <Day
-                                    key={day._id}
-                                    day={day}
-                                    dayChanged={(day) => dayChanged(day, index)}
-                                    index={index}
-                                    exerciseMap={exerciseMap}
-                                    onDelete={() => handleDelete(index)}
-                                />
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
-            <div className={styles.addDay}>
-                <button
-                    className={'standard-button'}
-                    onClick={() => setCopyDayVisible(true)}
-                >
-                    <i className='fa-solid fa-copy'></i>
-                    Copy Day
-                </button>
-                <button
-                    className={'standard-button primary'}
-                    onClick={handleAdd}
-                >
-                    <i className='fa-solid fa-plus'></i>
-                    Add Day
-                </button>
-            </div>
-            {copyDayVisible && (
-                <CopyDayBottomSheet days={days} onResult={onCopyDayResult} />
-            )}
-        </div>
-    );
-}
-
-interface DayProps {
     day: IWorkoutScheduleDay;
     dayChanged: (day: IWorkoutScheduleDay) => void;
     index: number;
@@ -164,7 +18,13 @@ interface DayProps {
     onDelete: () => void;
 }
 
-function Day({ day, dayChanged, index, exerciseMap, onDelete }: DayProps) {
+export default function WorkoutPlanDayEditor({
+    day,
+    dayChanged,
+    index,
+    exerciseMap,
+    onDelete,
+}: Props) {
     const [exercisePickerVisible, setExercisePickerVisible] = useState(false);
 
     const exercisesChanged = useCallback(
