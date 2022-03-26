a21e2f5839614796de87e3c362bfd21504ac009e
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-26 15:34:48 -0500

    fixed a couple css bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import React, { FC, Suspense } from 'react';
 
 import RouteLoader from './components/RouteLoader/RouteLoader';
@@ -13,61 +13,61 @@ const Home = React.lazy(() => import('./pages/home'));
 const Exercises = React.lazy(() => import('./pages/exercises'));
 const Exercise = React.lazy(() => import('./pages/exercise'));
 
-const App: FC = () => (
-    <>
-        <Routes>
-            <Route
-                path='home'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <Home />
-                    </Suspense>
-                }
-            />
-            <Route
-                path='exercises'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <Exercises />
-                    </Suspense>
-                }
-            />
-            <Route
-                path='exercises/:exerciseName'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <Exercise />
-                    </Suspense>
-                }
-            />
-            <Route
-                path='history'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <History />
-                    </Suspense>
-                }
-            />
-            <Route
-                path='settings'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <Settings />
-                    </Suspense>
-                }
-            />
-            <Route
-                path='workout-plan'
-                element={
-                    <Suspense fallback={<RouteLoader />}>
-                        <WorkoutPlan />
-                    </Suspense>
-                }
-            />
-            <Route path='*' element={<Navigate replace to='/home' />} />
-        </Routes>
-        <ServiceWorkerUpdater />
-    </>
-);
-
-export default App;
+export default function App() {
+    return (
+        <>
+            <Routes>
+                <Route
+                    path='home'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <Home />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='exercises'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <Exercises />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='exercises/:exerciseName'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <Exercise />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='history'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <History />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='settings'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <Settings />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='workout-plan'
+                    element={
+                        <Suspense fallback={<RouteLoader />}>
+                            <WorkoutPlan />
+                        </Suspense>
+                    }
+                />
+                <Route path='*' element={<Navigate replace to='/home' />} />
+            </Routes>
+            <ServiceWorkerUpdater />
+        </>
+    );
+}
